c52c6538fab9f5296bb2ef2864b778cdf00d8cb3
LAPTOP-N7931AAU\franc <francisco.perez@simplemoviles.com>
2022-10-07 17:11:25 -0300

    Habilitar CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import app from './app.js'
-import cors from 'cors';
+import cors from 'cors'
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors())
 
 app.listen(app.get('port'))
- 
-console.log('Server on Port',app.get('port'))
+
+console.log('Server on Port', app.get('port'))
